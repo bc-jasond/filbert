2a350d1d20c3ac62029a43eb7f0a2fabb028ac88
Jason Dubaniewicz <jason@reviewsnap.com>
2019-01-02 08:36:44 -0800

    add Eric Meyer`s CSS Reset

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
 
 import styled from 'styled-components';
+import CssReset from './reset.css';
+
+const Header = styled.header``;
+const Article = styled.article``;
+const Footer = styled.footer``;
 
 const H1 = styled.h1``;
 
 const App = () => (
-  <H1>Hello World!!</H1>
+  <React.Fragment>
+    <Header>
+      Header
+    </Header>
+    <Article>
+      <H1>Hello World!!</H1>
+    </Article>
+    <Footer>
+      Footer
+    </Footer>
+    <CssReset/>
+  </React.Fragment>
 );
 
 const AppWithHot = hot(module)(App);
