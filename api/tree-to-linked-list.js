7318135b976abe047d1daad4223e82b635166b1a
Jason Dubaniewicz <jasondebo@gmail.com>
2019-12-04 19:45:11 -0800

    conversion script bug, setup to run on all posts

--- api/tree-to-linked-list.js
+++ api/tree-to-linked-list.js
@@ -12,7 +12,7 @@ const {
 
 const { getMapWithId } = require('../frontend/src/common/utils')
 
-async function main() {
+async function main(ids) {
   try {
     console.info(
       figlet.textSync(
@@ -22,72 +22,74 @@ async function main() {
         })
     )
     const knex = await getKnex();
-    const id = 7;
-    const [post] = await knex('post')
-      .where({
-        id,
-      });
-    if (!post) {
-      console.log("bad post id", id);
-      return;
-    }
-    // NOTE: uses old nodesByParentId ordered by position
-    const contentNodes = await getNodes(knex, post.id);
-    // create new post
-    //const newPostId = 999;
-    const [newPostId] = await knex
-     .insert({ user_id: 1, title: 'placeholder post', canonical: 'some-url-here' })
-     .into('post');
-    
-    let updates = Map();
-    // updates.push(["34ab", { post_id: newPostId, node: Map() }])
-    const nodesByParentId = fromJS(JSON.parse(JSON.stringify(contentNodes)));
-    const rootId = nodesByParentId.get("null").first().get('id')
-    let last;
-    
-    // DFS to get the proper top-down ordering
-    function inner(nodeId) {
-      const nodes = nodesByParentId.get(nodeId);
-      nodes.forEach(node => {
-        node = node.delete('parent_id').delete('position').set('post_id', newPostId);
-        if (nodesByParentId.get(node.get('id'))) {
-          inner(node.get('id'));
-          return;
-        }
-        if (node.get('type') === 'codesection') {
-          const lines = node.getIn(['meta', 'lines']);
-          lines.forEach(line => {
-            const newNode = getMapWithId({ content: line, type: 'pre', post_id: newPostId })
-            if (last) {
-              updates = updates.set(last.get('id'), last.set('next_sibling_id', newNode.get('id')))
-            }
-            updates = updates.set(newNode.get('id'), newNode);
-            last = newNode;
-          })
-          return;
-        }
-        if (last) {
-          updates = updates.set(last.get('id'), last.set('next_sibling_id', node.get('id')))
-        }
-        updates = updates.set(node.get('id'), node.set('post_id', newPostId));
-        last = node;
-      })
-    }
-    
-    inner(rootId);
-    try {
-      await bulkContentNodeUpsert(
-        updates.map((v, k) => List([
-            k,
-            {
-              post_id: newPostId,
-              node: v.toJS(),
-            }
-          ])
-        ).toList().toJS()
-      )
-    } catch (err) {
-      console.error(err)
+    for (let i = 0; i < ids.length; i++) {
+      const id = ids[i];
+      const [post] = await knex('post')
+        .where({
+          id,
+        });
+      if (!post) {
+        console.log("bad post id", id);
+        return;
+      }
+      // NOTE: uses old nodesByParentId ordered by position
+      const contentNodes = await getNodes(knex, post.id);
+      // create new post
+      //const newPostId = 999;
+      const [newPostId] = await knex
+        .insert({ user_id: 1, title: post.title, canonical: 'some-url-here' })
+        .into('post');
+  
+      let updates = Map();
+      // updates.push(["34ab", { post_id: newPostId, node: Map() }])
+      const nodesByParentId = fromJS(JSON.parse(JSON.stringify(contentNodes)));
+      const rootId = nodesByParentId.get("null").first().get('id')
+      let last;
+  
+      // DFS to get the proper top-down ordering
+      function inner(nodeId) {
+        const nodes = nodesByParentId.get(nodeId);
+        nodes.forEach(node => {
+          node = node.delete('parent_id').delete('position').set('post_id', newPostId);
+          if (nodesByParentId.get(node.get('id'))) {
+            inner(node.get('id'));
+            return;
+          }
+          if (node.get('type') === 'codesection') {
+            const lines = node.getIn(['meta', 'lines'], List());
+            lines.forEach(line => {
+              const newNode = getMapWithId({ content: line, type: 'pre', post_id: newPostId })
+              if (last) {
+                updates = updates.set(last.get('id'), last.set('next_sibling_id', newNode.get('id')))
+              }
+              updates = updates.set(newNode.get('id'), newNode);
+              last = newNode;
+            })
+            return;
+          }
+          if (last) {
+            updates = updates.set(last.get('id'), last.set('next_sibling_id', node.get('id')))
+          }
+          updates = updates.set(node.get('id'), node.set('post_id', newPostId));
+          last = node;
+        })
+      }
+  
+      inner(rootId);
+      try {
+        await bulkContentNodeUpsert(
+          updates.map((v, k) => List([
+              k,
+              {
+                post_id: newPostId,
+                node: v.toJS(),
+              }
+            ])
+          ).toList().toJS()
+        )
+      } catch (err) {
+        console.error(err)
+      }
     }
     console.info("Filbert Conversion Finished 👍")
     process.exit(0);
@@ -97,5 +99,5 @@ async function main() {
   }
 }
 
-main();
+main([1,2,3,4,5,6,7,8,160]);
 
