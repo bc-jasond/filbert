d253819e0c38eda979d1af3ed9e4b259c627c68e
Jason Dubaniewicz <jasondebo@gmail.com>
2020-01-02 17:29:06 -0800

    add s3 client & basic wrapper methods

--- api/filbert-cron.js
+++ api/filbert-cron.js
@@ -1,92 +1,64 @@
-const { exec } = require('child_process');
+const { promisify } = require('util');
+const {exec: execCb} = require('child_process');
+const exec = promisify(execCb);
 
 const cron = require('node-cron');
 
 const bucketPrefix = `filbert-${process.env.NODE_ENV || 'dev'}-mysqlbackup`;
 const hourlyBucketName = `${bucketPrefix}-hourly`;
 const dailyBucketName = `${bucketPrefix}-daily`;
-const adhocBucketName = `${bucketPrefix}-adhoc`;
+const adhocBucketName = `filbert-mysql-backups`;
 
 const testHourly = './hourly';
 const testDaily = './daily';
 
-function cleanupBadFile(filenameAndPath) {
-  exec(`rm ${filenameAndPath}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error('rm failed mysqldump file error: ', error);
-      return;
-    }
-    console.log('cleaned up failed mysqldump file: ', filenameAndPath);
+async function wrapExec(command) {
+  try {
+    const {stdout, stderr} = await exec(command);
+    console.log(`exec() command succeeded: ${command}`)
     if (stdout) console.log(stdout);
-    if (stderr) console.log(stderr);
-  });
+    // TODO: use mysql_editor_config to store obfuscated credentials in a .mylogin.cnf
+    //  but, the percona docker doesn't create a /home/mysql dir, so need to investigate
+    // https://dev.mysql.com/doc/refman/5.6/en/mysql-config-editor.html
+    if (stderr && !stderr.includes('password')) console.error(stderr);
+    return stdout || true;
+  }
+  catch(err) {
+    console.error(`exec() command failed: ${command}`)
+  }
+}
+
+async function cleanupBadFile(filenameAndPath) {
+  return wrapExec(`rm ${filenameAndPath}`);
 }
 
 async function makeMysqlDump(now) {
   const currentBackupFilename = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}_${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}${now.getSeconds().toString().padStart(2, '0')}.sql`;
   const currentFileAndPath = `${testHourly}/${currentBackupFilename}`;
   try {
-    const {stdout, stderr} = await new Promise(async (resolve, reject) => {
-      exec(`docker exec $PERCONA_CONTAINER_NAME /usr/bin/mysqldump --hex-blob --default-character-set=utf8mb4 --databases filbert -uroot -p"$MYSQL_ROOT_PASSWORD" > ${currentFileAndPath}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`mysqldump error: ${error}`);
-          reject(error);
-          return
-        }
-        resolve({stdout, stderr});
-      })
-    });
-    if (stdout) console.log(stdout);
-    // TODO: use mysql_editor_config to store obfuscated credentials in a .mylogin.cnf
-    //  but, the percona docker doesn't create a /home/mysql dir, so need to investigate
-    // https://dev.mysql.com/doc/refman/5.6/en/mysql-config-editor.html
-    if (stderr && !stderr.includes('password')) console.error(stderr);
+    const stdout = await wrapExec(`docker exec $PERCONA_CONTAINER_NAME /usr/bin/mysqldump --hex-blob --default-character-set=utf8mb4 --databases filbert -uroot -p"$MYSQL_ROOT_PASSWORD" > ${currentFileAndPath}`);
+    if (typeof stdout === 'string') console.log(stdout);
     return {filename: currentBackupFilename, path: testHourly};
   } catch (error) {
-    cleanupBadFile(currentFileAndPath);
+    return cleanupBadFile(currentFileAndPath);
   }
 }
 
 async function lsDateDesc(path) {
-  return new Promise((resolve, reject) => {
-    exec(`ls -td $PWD/${path}/*`, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      if (stderr) console.error(stderr);
-      resolve(stdout.trim().split('\n'));
-    })
-  })
+  const stdout = await wrapExec(`ls -td $PWD/${path}/*`);
+  return stdout.trim().split('\n');
 }
 
 async function copyFile(file, newPath) {
-  return new Promise((resolve, reject) => {
-    exec(`cp ${file} ${newPath}`, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      if (stderr) console.error(stderr);
-      resolve(stdout);
-    })
-  })
+  return wrapExec(`cp ${file} ${newPath}`);
 }
 
 async function deleteFiles(files) {
-  return new Promise((resolve, reject) => {
-    if (files.length === 0) {
-      resolve();
-    }
-    exec(`rm ${files.join(' ')}`, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      if (stderr) console.error(stderr);
-      resolve(stdout);
-    })
-  })
+  if (files.length === 0) {
+    return;
+  }
+  console.log(`deleting ${files.length} files: `, files)
+  return wrapExec(`rm ${files.join(' ')}`)
 }
 
 /**
@@ -115,7 +87,6 @@ cron.schedule('*/3 * * * * *', async () => {
     const filesInDaily = await lsDateDesc(testDaily);
     // keep a max window by deleting older backups
     const dailyBackupsToDelete = filesInDaily.slice(numberOfDailyBackupsToKeep)
-    console.log(`daily: deleting ${dailyBackupsToDelete.length} files: `, dailyBackupsToDelete)
     await deleteFiles(dailyBackupsToDelete)
   }
   // keep a max window by deleting older backups
