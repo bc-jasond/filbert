9d3ac1b95d5584ed5a6708c70d440f12f8a712f3
Jason Dubaniewicz <jasondebo@gmail.com>
2020-04-17 06:18:23 -0700

    app.jsx -> React Hooks

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { getSession } from './common/session';
@@ -21,102 +21,85 @@ import ViewPost from './pages/view-post';
 import UserProfile from './pages/user-profile';
 import Publish from './pages/publish';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      session: getSession(),
-    };
-  }
-
-  setSession = (session) => {
-    this.setState({ session });
-  };
-
-  render() {
-    const {
-      state: { session },
-    } = this;
-    const username = session.get('username');
-    const RouteWithSession = ({
-      component: Component,
-      exact,
-      path,
-      requiresAuth,
-      shouldListDrafts,
-    }) => {
-      if (requiresAuth && !username) {
-        return <Page404 />;
-      }
-      return (
-        <Route
-          exact={exact}
-          path={path}
-          render={({ match: { params = {} } }) => (
-            <Component
-              params={params}
-              session={session}
-              setSession={this.setSession}
-              shouldListDrafts={shouldListDrafts}
-            />
-          )}
-        />
-      );
-    };
+export default React.memo(() => {
+  const [session, setSession] = useState(getSession());
+  const username = session.get('username');
+  const RouteWithSession = ({
+    component: Component,
+    exact,
+    path,
+    requiresAuth,
+    shouldListDrafts,
+  }) => {
+    if (requiresAuth && !username) {
+      return <Page404 />;
+    }
     return (
-      <>
-        <BrowserRouter>
-          <Switch>
-            <Redirect push exact from="/p" to="/public" />
-            <Redirect push exact from="/" to="/p/homepage" />
-            <Redirect push exact from="/help" to="/p/help" />
-            <Redirect push exact from="/about" to="/p/about" />
-            {username && (
-              <Redirect push exact from="/me" to={`/@${username}`} />
-            )}
-
-            <RouteWithSession exact path="/signout" component={Signout} />
-            <RouteWithSession exact path="/signin" component={SignIn} />
-            {/* NOTE: signin admin doesn't have a link in, you have to know the url */}
-            <RouteWithSession
-              exact
-              path="/signin-admin"
-              component={SignInAdmin}
-            />
-            <RouteWithSession exact path="/public" component={PostList} />
-            <RouteWithSession
-              exact
-              path="/private"
-              component={PostList}
-              requiresAuth
-              shouldListDrafts
-            />
-            {/* NOTE: view a "published" post - this :canonical is a string like: 'some-url-87ba' */}
-            <RouteWithSession exact path="/p/:canonical" component={ViewPost} />
-            {/* NOTE: this :id is an int like: 34 */}
-            <RouteWithSession
-              exact
-              path="/edit/:id"
-              component={EditPost}
-              requiresAuth
-            />
-            <RouteWithSession
-              exact
-              path="/publish/:id"
-              component={Publish}
-              requiresAuth
-            />
-            <RouteWithSession exact path="/:username" component={UserProfile} />
-
-            <RouteWithSession component={Page404} />
-          </Switch>
-        </BrowserRouter>
-        <CssReset />
-        <CssFonts />
-        <CssVariables />
-        <CssGlobals />
-        <CssPace />
-      </>
+      <Route
+        exact={exact}
+        path={path}
+        render={({ match: { params = {} } }) => (
+          <Component
+            params={params}
+            session={session}
+            setSession={setSession}
+            shouldListDrafts={shouldListDrafts}
+          />
+        )}
+      />
     );
-  }
-}
+  };
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Redirect push exact from="/p" to="/public" />
+          <Redirect push exact from="/" to="/p/homepage" />
+          <Redirect push exact from="/help" to="/p/help" />
+          <Redirect push exact from="/about" to="/p/about" />
+          {username && <Redirect push exact from="/me" to={`/@${username}`} />}
+
+          <RouteWithSession exact path="/signout" component={Signout} />
+          <RouteWithSession exact path="/signin" component={SignIn} />
+          {/* NOTE: signin admin doesn't have a link in, you have to know the url */}
+          <RouteWithSession
+            exact
+            path="/signin-admin"
+            component={SignInAdmin}
+          />
+          <RouteWithSession exact path="/public" component={PostList} />
+          <RouteWithSession
+            exact
+            path="/private"
+            component={PostList}
+            requiresAuth
+            shouldListDrafts
+          />
+          {/* NOTE: view a "published" post - this :canonical is a string like: 'some-url-87ba' */}
+          <RouteWithSession exact path="/p/:canonical" component={ViewPost} />
+          {/* NOTE: this :id is an int like: 34 */}
+          <RouteWithSession
+            exact
+            path="/edit/:id"
+            component={EditPost}
+            requiresAuth
+          />
+          <RouteWithSession
+            exact
+            path="/publish/:id"
+            component={Publish}
+            requiresAuth
+          />
+          <RouteWithSession exact path="/:username" component={UserProfile} />
+
+          <RouteWithSession component={Page404} />
+        </Switch>
+      </BrowserRouter>
+      <CssReset />
+      <CssFonts />
+      <CssVariables />
+      <CssGlobals />
+      <CssPace />
+    </>
+  );
+});
