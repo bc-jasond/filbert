20c9765abee212f05e2bde2ad4baab7fcd7b14f1
Jason Dubaniewicz <jason@filbert.xyz>
2020-10-20 20:52:54 -0700

    bye bye React - delete all of React in this commit

--- frontend/src/app-hot.jsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable-next-line import/no-extraneous-dependencies */
-import { hot } from 'react-hot-loader';
-import App from './app';
-
-export default hot(module)(App);
--- frontend/src/css.js
+++
@@ -1,4 +0,0 @@
-export const bezier = (property = 'all') =>
-  `transition: ${property} 0.3s cubic-bezier(0.175, 0.885, 0.320, 1.275);`;
-export const ease = (property = 'all') =>
-  `transition: ${property} 0.125s ease-out`;
